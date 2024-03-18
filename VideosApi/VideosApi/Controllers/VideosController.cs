012f936148144df31e961d798efde9e2546476b6
Artem Drahun <a.o.drahun@student.khai.edu>
2024-03-18 18:23:00 +0200

    Added parameters to PhysicalFile result and formatted code

--- VideosApi/VideosApi/Controllers/VideosController.cs
+++ VideosApi/VideosApi/Controllers/VideosController.cs
@@ -7,57 +7,72 @@ using VideosApi.Database;
 using VideosApi.Models;
 using VideosApi.Services;
 using System.IO;
+using System.Net.Mime;
 
 namespace VideosApi.Controllers;
 
 [ApiController]
 [Route("/api/videos")]
-public class VideosController(ILogger<VideosController> logger, VideosDbContext dbContext, ConversionQueueService conversionQueueService, IServiceScopeFactory scopeFactory): ControllerBase
-{   
+public class VideosController(
+    ILogger<VideosController> logger,
+    VideosDbContext dbContext,
+    ConversionQueueService conversionQueueService,
+    IServiceScopeFactory scopeFactory) : ControllerBase
+{
     private const string FilesPath = @"C:\Users\leviaweek\Desktop\2314226";
-    
+
     [HttpGet]
     public async Task<Ok<GetVideosResponse>> GetVideosListAsync(CancellationToken cancellationToken = default)
     {
-        var videos = await dbContext.VideosInfos.Where(video => video.IsUploaded).OrderBy(video =>  video.Id).Take(10).Select<VideoInfo, VideoCard>(video =>
-            new VideoCard
-            {
-                Id = video.Id,
-                Title = video.Title,
-                Duration = video.PhysicalVideo.Duration,
-                CreatedAt = video.PhysicalVideo.CreatedAt
-            }).ToListAsync(cancellationToken: cancellationToken);
+        var videos = await dbContext.VideosInfos.Where(video => video.IsUploaded).OrderBy(video => video.Id).Take(10)
+            .Select<VideoInfo, VideoCard>(video =>
+                new VideoCard
+                {
+                    Id = video.Id,
+                    Title = video.Title,
+                    Duration = video.PhysicalVideo.Duration,
+                    CreatedAt = video.PhysicalVideo.CreatedAt
+                }).ToListAsync(cancellationToken: cancellationToken);
         var response = new GetVideosResponse(videos.Count, videos);
         logger.LogInformation("Sent: {response}", response.ToString());
         return TypedResults.Ok(response);
     }
-    [HttpGet("{id}")]
 
-    public async Task<Results<PhysicalFileHttpResult, NotFound>> GetVideoAsync(string id, CancellationToken cancellationToken = default)
+    [HttpGet("{id}")]
+    public async Task<Results<PhysicalFileHttpResult, NotFound>> GetVideoAsync(string id,
+        CancellationToken cancellationToken = default)
     {
-        var result = await dbContext.VideosInfos.FirstOrDefaultAsync(v => v.Id == id, cancellationToken: cancellationToken);
+        var result =
+            await dbContext.VideosInfos.FirstOrDefaultAsync(v => v.Id == id, cancellationToken: cancellationToken);
         if (result is null || !result.IsUploaded)
         {
             return TypedResults.NotFound();
         }
+
         logger.LogInformation("Sent video by id: {id}", id);
-        return TypedResults.PhysicalFile(Path.Combine(FilesPath, result.Id, "video.mp4"));
+        return TypedResults.PhysicalFile(Path.Combine(FilesPath, result.Id, "video.mp4"), "video/mp4",
+            fileDownloadName: $"{result.Id}.mp4", enableRangeProcessing: true);
     }
 
     [HttpGet("{id}/preview")]
-    public async Task<Results<PhysicalFileHttpResult, NotFound>> GetPreviewAsync(string id, CancellationToken cancellationToken = default)
+    public async Task<Results<PhysicalFileHttpResult, NotFound>> GetPreviewAsync(string id,
+        CancellationToken cancellationToken = default)
     {
-        var result = await dbContext.VideosInfos.FirstOrDefaultAsync(v => v.Id == id, cancellationToken: cancellationToken);
+        var result =
+            await dbContext.VideosInfos.FirstOrDefaultAsync(v => v.Id == id, cancellationToken: cancellationToken);
         if (result is null || !result.IsUploaded)
         {
             return TypedResults.NotFound();
         }
+
         logger.LogInformation("Sent preview by id: {id}", id);
-        return TypedResults.PhysicalFile(Path.Combine(FilesPath, result.Id, "preview.jpg"));
+        return TypedResults.PhysicalFile(Path.Combine(FilesPath, result.Id, "preview.jpg"), "image/jpeg",
+            fileDownloadName: $"{result.Id}.jpg");
     }
 
     [HttpPost]
-    public async Task<Ok<string>> PostVideoMetadataAsync([FromForm] PostFileMetadata postFileMetadata, CancellationToken cancellationToken = default)
+    public async Task<Ok<string>> PostVideoMetadataAsync([FromForm] PostFileMetadata postFileMetadata,
+        CancellationToken cancellationToken = default)
     {
         var video = new VideoInfo
         {
@@ -70,7 +85,8 @@ public class VideosController(ILogger<VideosController> logger, VideosDbContext 
     }
 
     [HttpPost("{id}")]
-    public async Task<Results<Ok<string>, NotFound<string>>> PostVideoAsync([FromForm] IFormFile formFile, string id, CancellationToken cancellationToken = default)
+    public async Task<Results<Ok<string>, NotFound<string>>> PostVideoAsync([FromForm] IFormFile formFile, string id,
+        CancellationToken cancellationToken = default)
     {
         logger.LogInformation("{info}", formFile.Name);
         logger.LogInformation("{info}", id);
@@ -78,6 +94,7 @@ public class VideosController(ILogger<VideosController> logger, VideosDbContext 
         {
             return TypedResults.NotFound("Not a video content");
         }
+
         var video = await dbContext.VideosInfos.FirstOrDefaultAsync(v => v.Id == id, cancellationToken);
         if (video is null)
         {
@@ -91,7 +108,7 @@ public class VideosController(ILogger<VideosController> logger, VideosDbContext 
         var taskId = conversionQueueService.AddTask(task);
         return TypedResults.Ok(taskId.ToString("N"));
     }
-    
+
     [HttpGet("{id}/status")]
     public Results<Ok<ConversionTaskDto>, NotFound> GetVideoUploadingStatus(string id)
     {
@@ -100,6 +117,7 @@ public class VideosController(ILogger<VideosController> logger, VideosDbContext 
         {
             return TypedResults.NotFound();
         }
+
         var dto = new ConversionTaskDto
         {
             Progress = task.Progress,
